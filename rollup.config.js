9b4b8e4181a35bc7e1cabf1de9e66153a28f0a3f
Kristofer Baxter <kristofer@kristoferbaxter.com>
2018-02-13 16:19:55 -0800

    Removed webpack, moved to latest versions of dependencies, moved minification to rollup.

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,58 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
+let nameCache = {};
+let minify = false;
 
 export default {
-	entry: 'src/index.js',
-	dest: 'build/app.js',
-	format: 'iife',
-	sourceMap: true,
+	input: 'src/index.js',
+	output: {
+		file: 'build/app.js',
+		format: 'iife',
+		sourcemap: true,
+	},
 	external: [],
 	plugins: [
 		babel({
 			babelrc: false,
 			presets: [
-				['es2015', { loose:true, modules:false }],
-				'stage-0'
+				[
+					'@babel/env', {
+						targets: {
+							browsers: ['last 2 versions', 'ie >= 11', 'safari >= 7'],
+						},
+						loose: true,
+						modules: false
+					}
+				]
 			],
 			plugins: [
-				'external-helpers',
-				['transform-react-jsx', { pragma:'h' }]
+				['@babel/plugin-proposal-object-rest-spread'],
+				['@babel/proposal-class-properties'],
+				[ 
+					'@babel/transform-react-jsx', { 
+						pragma: 'h'
+					}
+				]
 			]
 		}),
 		nodeResolve({ jsnext:true }),
-		commonjs()
+		commonjs(),
+		minify && uglify({
+			output: { comments: false },
+			compress: {
+				keep_infinity: true,
+				pure_getters: true
+			},
+			warnings: true,
+			ecma: 5,
+			toplevel: false,
+			mangle: {
+				properties: false
+			},
+			nameCache
+		})
 	]
 };
